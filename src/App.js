991d6c5d2027868acdea897deb424fc9b0543fc9
SelterEoin <220375@student.pwr.edu.pl>
2017-12-13 14:26:12 +0100

    1.0.1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component }  from 'react';
 import { MegadraftEditor, MegadraftIcons, MyPageLinkIcon } from 'megadraft';
-var ReactDOM = require('react-dom');
 import {Editor, EditorState,RichUtils, convertToRaw,getDefaultKeyBinding, KeyBindingUtil} from 'draft-js';
-const {hasCommandModifier} = KeyBindingUtil;
 import {stateToHTML} from 'draft-js-export-html';
 import {stateFromHTML} from 'draft-js-import-html';
 import Graph from "react-graph-vis";
 
+const {hasCommandModifier} = KeyBindingUtil;
+
+var ReactDOM = require('react-dom');
 var axios = require('axios');
 var qs = require('qs');
-
 
 export default class App extends React.Component {
 
@@ -21,8 +21,7 @@ export default class App extends React.Component {
     this.onClick = this.handleClick.bind(this);
   }
 /*-----------------------------------------------------------------*/
-
-GRAPH_DOM = (props) => {
+GRAPH = (props) => {
 const graph = {
   nodes: [
     { id: 1, label: "Gun"},
@@ -30,11 +29,9 @@ const graph = {
     { id: 3, label: "AutomaticPistol"},
     { id: 4, label: "Revolver"},
     { id: 5, label: "Caliber"}
-
   ],
   edges: [{ from: 1, to: 2 }, { from: 2, to: 3 }, { from: 2, to: 4 }]
 };
-
 const options = {
   edges: {
     color: "#000000",
@@ -42,7 +39,6 @@ const options = {
     smooth: true,
   }
 };
-
 const events = {
   select: function(event) {
     var { nodes, edges } = event;
@@ -52,70 +48,66 @@ const events = {
     console.log(edges);
   }
 };
-
 ReactDOM.render(
   <div>
-    <Graph graph={graph} options={options} events={events} style={{ height: "240px" }} />
+    <Graph graph={graph} options={options} events={events} style={{ height: "220px" }} />
   </div>,
-  document.getElementById("GRAPH")
+  document.getElementById("graph_div")
 );
 }
-  /*------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------*/
   handleClick = (event) => {
     const {editorState} = this.state;
     var selectionState = editorState.getSelection();
-    var id_kursora = selectionState.focusOffset
+    var cursor_id = selectionState.focusOffset
     var anchorKey = selectionState.getAnchorKey();
     var currentContent = editorState.getCurrentContent();
     var currentContentBlock = currentContent.getBlockForKey(anchorKey);
     var text = currentContentBlock.getText();
-    console.log(id_kursora+text)
     axios.get('/one_word', {
-    params: {
-      ID: id_kursora,
-      content: text
-    }
+    params: {ID: cursor_id,content: text}
   })
-.then(function(response){
-  var dataforwordRAW_subclass = response.data.subclass_of_list_response;
-  var dataforwordLIST_subclass = dataforwordRAW_subclass.map(function(variable)
-{return <li><a href = '#'>{variable}</a></li>;})
-ReactDOM.render(<ul>{dataforwordLIST_subclass}</ul>,document.getElementById('SUBCLASSES_CLASS'))
 
-var dataforwordRAW_class_above = response.data.classes_above_list_response;
-var dataforwordLIST_class_above = dataforwordRAW_class_above.map(function(variable)
-{return <li><a href = '#'>{variable}</a></li>;})
-ReactDOM.render(<ul>{dataforwordLIST_class_above}</ul>,document.getElementById('UPPERCLASSES_CLASS'))
+  .then(function(response){
+    var subclass = response.data.subclasses_of_class_response;
+    var mapped_subclass = subclass.map(function(variable)
+  {return <li><a href = '#'>{variable}</a></li>;})
+  ReactDOM.render(<ul>{mapped_subclass}</ul>,document.getElementById('subclasses_of_class_div'))
 
-var dataforwordRAW_INDI_CLASS = response.data.instances_from_the_clicked_class_list_response;
-var dataforwordLIST_INDI_CLASS = dataforwordRAW_INDI_CLASS.map(function(variable)
-{return <li><a href = '#'>{variable}</a></li>;})
-ReactDOM.render(<ul>{dataforwordLIST_INDI_CLASS}</ul>,document.getElementById('INDI_CLASS'))
+  var class_above = response.data.classes_above_class_response;
+  var mapped_class_above = class_above.map(function(variable)
+  {return <li><a href = '#'>{variable}</a></li>;})
+  ReactDOM.render(<ul>{mapped_class_above}</ul>,document.getElementById('classes_above_class_div'))
 
-var dataforwordRAW_CLASS_INDI = response.data.sorted_above_INDI_list_response;
-var dataforwordLIST_CLASS_INDI = dataforwordRAW_CLASS_INDI.map(function(variable)
-{return <li><a href = '#'>{variable}</a></li>;})
-ReactDOM.render(<ul>{dataforwordLIST_CLASS_INDI}</ul>,document.getElementById('CLASS_INDI'))
+  var individuals_of_class = response.data.individuals_of_class_response;
+  var mapped_individuals_of_class = individuals_of_class.map(function(variable)
+  {return <li><a href = '#'>{variable}</a></li>;})
+  ReactDOM.render(<ul>{mapped_individuals_of_class}</ul>,document.getElementById('individuals_of_class_div'))
 
-var dataforwordRAW_INDI_INDI = response.data.instances_from_the_same_class_list_response;
-var dataforwordLIST_INDI_INDI = dataforwordRAW_INDI_INDI.map(function(variable)
-{return <li><a href = '#'>{variable}</a></li>;})
-ReactDOM.render(<ul>{dataforwordLIST_INDI_INDI}</ul>,document.getElementById('INDI_INDI'))
+  var classes_of_individual = response.data.classes_of_individual_response;
+  var mapped_classes_of_individual = classes_of_individual.map(function(variable)
+  {return <li><a href = '#'>{variable}</a></li>;})
+  ReactDOM.render(<ul>{mapped_classes_of_individual}</ul>,document.getElementById('classes_of_individual_div'))
 
-var dataforwordRAW_REL_INDI = response.data.relations_result_list_response;
-var dataforwordLIST_REL_INDI = dataforwordRAW_REL_INDI.map(function(variable)
-{return <li><a href = '#'>{variable}</a></li>;})
-ReactDOM.render(<ul>{dataforwordLIST_REL_INDI}</ul>,document.getElementById('REL_INDI'))
-})
+  var twin_individual = response.data.twin_individual_response;
+  var mapped_twin_individual = twin_individual.map(function(variable)
+  {return <li><a href = '#'>{variable}</a></li>;})
+  ReactDOM.render(<ul>{mapped_twin_individual}</ul>,document.getElementById('twin_individual_div'))
+
+  var relations = response.data.relations_response;
+  var mapped_relations = relations.map(function(variable)
+  {return <li><a href = '#'>{variable}</a></li>;})
+  ReactDOM.render(<ul>{mapped_relations}</ul>,document.getElementById('relations_div'))
+  })
+
   .catch(function (error) {
     console.log(error);
   });
   }
-  /*-------------------------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------------------------*/
   _search_all(props){
     const {editorState} = this.state;
     var selectionState = editorState.getSelection();
-    var id_kursora = selectionState.focusOffset
     var anchorKey = selectionState.getAnchorKey();
     var currentContent = editorState.getCurrentContent();
     var currentContentBlock = currentContent.getBlockForKey(anchorKey);
@@ -123,105 +115,74 @@ ReactDOM.render(<ul>{dataforwordLIST_REL_INDI}</ul>,document.getElementById('REL
     let html = stateToHTML(currentContent);
     let contentState = stateFromHTML(html);
     var globalObject = this;
-    console.log(html)
-    console.log(text)
   axios.get('/search_all',{params: {content:text}})
         .then(function(response){
- var words_in = response.data.response_search_all_list
-console.log(words_in)
+ var words_in = response.data.search_all_response;
     var newHTMLContent = html;
     words_in.forEach(currentWord => {
-          console.log(words_in)
-          console.log(currentWord)
           newHTMLContent = newHTMLContent.replace(currentWord,'<strong>'+currentWord+'</strong>');
         });
     globalObject.setState({
       editorState: EditorState.createWithContent(stateFromHTML(newHTMLContent))
     });
- var words_in_list = words_in.map(function(variable){
- return <li><a href = '#'>{variable}</a></li>;
-});
-ReactDOM.render(<ul>{words_in_list}</ul>,document.getElementById('SEARCH_ALL_DOM'))
+
    }).catch(function(error){
      console.log(error);
    });
  }
-
-  /*-------------------------------------------------------------------------------------------*/
-
-  _data_OWL_CLASSES(props){
-  axios.get('/OWL_DATA_CLASSES',{})
+/*-------------------------------------------------------------------------------------------*/
+  _DISP_CLASSES(props){
+  axios.get('/DISP_ALL_CLASSES',{})
         .then(function(response){
-     console.log(response.data);
-   console.log(response.status);
-   console.log(response.statusText);
-   console.log(response.headers);
-   console.log(response.config);
- var dataCLASSES = response.data.dataCLASSESback
- var dataCLASSESlist = dataCLASSES.map(function(variable){
+ var classes = response.data.all_classes_response;
+ var mapped_classes = classes.map(function(variable){
  return <li><a href = '#'>{variable}</a></li>;
 });
-ReactDOM.render(<ul>{dataCLASSESlist}</ul>,document.getElementById('OWL_DATA_CLASSES'))
+ReactDOM.render(<ul>{mapped_classes}</ul>,document.getElementById('all_classes_from_ontology_div'))
    }).catch(function(error){
      console.log(error);
-
-	var tymczasowa_lista = ['TEST ERROR' , '2', '3', '4', '5','6','7']
-	var list = tymczasowa_lista.map(function(variable){
-	return <ol><a href = '#'>{variable}</a></ol>;
-	 });
-	  ReactDOM.render(<ul>{list}</ul>,document.getElementById('OWL_DATA_CLASSES'))
    });
  }
 /*-------------------------------------------------------------------------------*/
- _data_OWL_INDIVIDUALS(props){
- axios.get('/OWL_DATA_INDIVIDUALS',{})
+ _DISP_INDIVIDUALS(props){
+ axios.get('/DISP_ALL_INDIVIDUALS',{})
        .then(function(response){
-  console.log(response.data);
-  console.log(response.status);
-  console.log(response.statusText);
-  console.log(response.headers);
-  console.log(response.config);
-var dataIND = response.data.dataINDIback
-var dataINDlist = dataIND.map(function(variable){
+var individuals = response.data.all_individuals_response;
+var mapped_individuals = individuals.map(function(variable){
 return <li><a href = '#'>{variable}</a></li>;
 });
-ReactDOM.render(<ul>{dataINDlist}</ul>,document.getElementById('OWL_DATA_INDIVIDUALS'))
+ReactDOM.render(<ul>{mapped_individuals}</ul>,document.getElementById('all_individuals_from_ontology_div'))
   }).catch(function(error){
     console.log(error);
   });
 }
-  /*-------------------------------------------------------------------------------------------*/
-_data_OWL_OBJECT_PROPERTIES(props){
-axios.get('/OWL_DATA_OBJECT_PROPERTIES',{})
+/*-------------------------------------------------------------------------------------------*/
+_DISP_RELATIONS(props){
+axios.get('/DISP_ALL_RELACTIONS',{})
       .then(function(response){
-   console.log(response.data);
- console.log(response.status);
- console.log(response.statusText);
- console.log(response.headers);
- console.log(response.config);
-var dataPROPERTIES = response.data.dataPROPERTIESback
-var dataPROPERTIESlist = dataPROPERTIES.map(function(variable){
+var relations = response.data.all_relations_response;
+var mapped_relations = relations.map(function(variable){
 return <li><a href = '#'>{variable}</a></li>;
 });
-ReactDOM.render(<ul>{dataPROPERTIESlist}</ul>,document.getElementById('OWL_DATA_OBJECT_PROPERTIES'))
+ReactDOM.render(<ul>{mapped_relations}</ul>,document.getElementById('all_relations_from_ontology_div'))
  }).catch(function(error){
    console.log(error);
  });
 }
 /*-------------------------------------------------------------------------------------------*/
-_data_OWL_LIB1(props){
-axios.get('/OWL_DATA_LIB_button1',{})
+_LOAD_ONTOLOGY_1(props){
+axios.get('/LOAD_ONTOLOGY_1_IN_BACKEND',{})
       .then(function(response){
-ReactDOM.render(document.getElementById('LIB'))
+ReactDOM.render(document.getElementById('load_ontology_div'))
  }).catch(function(error){
 
  });
 }
 /*-------------------------------------------------------------------------------------------*/
-_data_OWL_LIB2(props){
-axios.get('/OWL_DATA_LIB_button2',{})
+_LOAD_ONTOLOGY_2(props){
+axios.get('/LOAD_ONTOLOGY_2_IN_BACKEND',{})
       .then(function(response){
-ReactDOM.render(document.getElementById('LIB'))
+ReactDOM.render(document.getElementById('load_ontology_div'))
  }).catch(function(error){
 
  });
@@ -240,7 +201,7 @@ ReactDOM.render(document.getElementById('LIB'))
       { type: "block", label: "QT", style: "blockquote", icon: MegadraftIcons.BlockQuoteIcon }
     ];
     return (
-    <div id='content'>
+    <div id='content_div'>
       <div className='editor' onClick={this.onClick}>
           <MegadraftEditor
       editorState={this.state.editorState}
@@ -252,45 +213,41 @@ ReactDOM.render(document.getElementById('LIB'))
       handlePastedText={this.handlePastedText}
       actions={megadraftActions}
         />
-        </div>
-		<div id  = 'wynik'>
-    <div id  = 'LIB'>
+    </div>
+		<div id  = 'result_div'>
+    <div id  = 'load_ontology_div'>
     Please choose which ontology do you want  to use.
-<button onClick={this._data_OWL_LIB1.bind(this)}> PEOPLE </button>
-<button onClick={this._data_OWL_LIB2.bind(this)}> GUNS </button>
+    <button onClick={this._LOAD_ONTOLOGY_1.bind(this)}> PEOPLE </button>
+    <button onClick={this._LOAD_ONTOLOGY_2.bind(this)}> GUNS </button>
     </div>
-    <div id = 'SEARCH_ALL_DOM_BUTTON'>
-    <button onClick={this._search_all.bind(this)}> Click to analize currently input text </button>
+    <div id = 'small_div'>
+    <button onClick={this._search_all.bind(this)}> Click here to analize text </button>
     </div>
-    <div id = 'SEARCH_ALL_DOM'> In this place all words that are in dictionary of choosen discipline, will display.</div>
-    <div id = 'GRAPH'>
-<button onClick={this.GRAPH_DOM.bind(this)}>Click to show subclasses of Gun</button>
-    </div>
-    <div id  = 'ONE_WORD_INFO'>If pressed word is a class - upperclasses of this class will display below.</div>
-    <div id  = 'UPPERCLASSES_CLASS'></div>
-    <div id  = 'ONE_WORD_INFO'>If pressed word is a class - subclasses of this class will display below.</div>
-    <div id  = 'SUBCLASSES_CLASS'></div>
-    <div id  = 'ONE_WORD_INFO'>If pressed word is a class - individuals of this class will display below.</div>
-    <div id  = 'INDI_CLASS'></div>
-    <div id  = 'ONE_WORD_INFO'>If pressed word is a individual - upperclasses of this individual will display below.</div>
-    <div id  = 'CLASS_INDI'></div>
-    <div id  = 'ONE_WORD_INFO'>If pressed word is a individual - siblings of this individual will display below.</div>
-    <div id  = 'INDI_INDI'></div>
-    <div id  = 'ONE_WORD_INFO'>If pressed word is a individual - relacions of this individual will display below.</div>
-    <div id  = 'REL_INDI'></div>
+    <div id  = 'big_div'>If chosen word is a class - its upperclasses will display here:</div>
+    <div id  = 'classes_above_class_div'></div>
+    <div id  = 'big_div'>If chosen word is a class - its subclasses will display here:</div>
+    <div id  = 'subclasses_of_class_div'></div>
+    <div id  = 'big_div'>If chosen word is a class - its individuals will display here:</div>
+    <div id  = 'individuals_of_class_div'></div>
+    <div id  = 'big_div'>If chosen word is an individual - its upperclasses will display here:</div>
+    <div id  = 'classes_of_individual_div'></div>
+    <div id  = 'big_div'>If chosen word is an individual - its siblings will display here:</div>
+    <div id  = 'twin_individual_div'></div>
+    <div id  = 'big_div'>If chosen word is an individual - its relations will display here:</div>
+    <div id  = 'relations_div'></div>
 
-    <div id = 'OWL_DATA_CLASSES_BUTTON'>
-    <button onClick={this._data_OWL_CLASSES.bind(this)}>Click to show Classes defined in the ontology</button>
+    <div id = 'small_div'><button onClick={this._DISP_CLASSES.bind(this)}>Click to show all classes defined in the ontology</button></div>
+    <div id  = 'all_classes_from_ontology_div'>Click the above button to see all classes defined in the ontology.</div>
+
+    <div id = 'big_div'><button onClick={this._DISP_INDIVIDUALS.bind(this)}>Click to show all individuals defined in the ontology</button></div>
+    <div id  = 'all_individuals_from_ontology_div'>Click the above button to see all individuals defined in the ontology.</div>
+
+    <div id = 'small_div'><button onClick={this._DISP_RELATIONS.bind(this)}>Click to show all relations defined in the ontology</button></div>
+    <div id  = 'all_relations_from_ontology_div'>Click the above button to see all relations defined in the ontology.</div>
+
+    <div id = 'graph_div'>
+    <button onClick={this.GRAPH.bind(this)}>Click here to render the graph</button>
     </div>
-    <div id  = 'OWL_DATA_CLASSES'>Click the above button to see all classes defined in the ontology.</div>
-    <div id = 'OWL_DATA_INDIVIDUALS_BUTTON'>
-    <button onClick={this._data_OWL_INDIVIDUALS.bind(this)}>Click to show The individuals (or instances) defined in the ontology</button>
-    </div>
-    <div id  = 'OWL_DATA_INDIVIDUALS'>Click the above button to see all individuals defined in the ontology.</div>
-    <div id = 'OWL_DATA_OBJECT_PROPERTIES_BUTTON'>
-    <button onClick={this._data_OWL_OBJECT_PROPERTIES.bind(this)}>Click to show ObjectProperties defined in the ontology</button>
-    </div>
-    <div id  = 'OWL_DATA_OBJECT_PROPERTIES'>Click the above button to see all object properties defined in the ontology.</div>
     </div>
     </div>
     );
